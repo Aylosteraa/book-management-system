b775791ad2d94b8f7d69ec05b134330d4e325279
Aylosteraa <barabashmarinaaa@gmail.com>
2026-06-05 14:17:01 +0300

    add config for PostgreSQL and SQLAlchemy

--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 
+from app.db.database import engine
+from sqlalchemy import text
+
 app = FastAPI(
     title="Book Management System"
 )
@@ -10,3 +13,13 @@ async def root():
     return {
         "message": "Hello World!!!!"
     }
+
+@app.get("/db")
+async def health_db():
+
+    async with engine.connect() as conn:
+        result = await conn.execute(
+            text("SELECT 1")
+        )
+
+    return {"result": result.scalar()}
